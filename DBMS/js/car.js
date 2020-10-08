d4fab3d187a336a2005f2e64f057652613824686
gayuvenkat <62341509+gayuvenkat@users.noreply.github.com>
2020-10-08 19:17:29 +0530

    Update car.js

--- DBMS/js/car.js
+++ DBMS/js/car.js
@@ -1,1 +1,17 @@
-document.getElementById("car-name").innerHTML = sessionStorage.getItem("NAME");
+  
+window.addEventListener('load', () => {
+
+    // Via Query parameters - GET
+    /* const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const surname = params.get('surname'); */
+
+    // Via local Storage
+    /* const name = localStorage.getItem('NAME');
+    const surname = localStorage.getItem('SURNAME'); */
+    
+    const name = sessionStorage.getItem('NAME');
+    
+    document.getElementById('result-name').innerHTML = name;
+
+})
